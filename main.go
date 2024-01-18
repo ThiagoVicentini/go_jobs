0dd7a0b9da5d4aadc43cba72cc64f65e385f4874
Anakin <thiago.h.vicentini@gmail.com>
2024-01-18 16:25:18 -0300

    feat: add config file and custom logger

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/ThiagoVicentini/go_jobs/router"
+import (
+	"github.com/ThiagoVicentini/go_jobs/config"
+	"github.com/ThiagoVicentini/go_jobs/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
